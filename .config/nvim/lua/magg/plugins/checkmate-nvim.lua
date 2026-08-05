4dc744a08622f53008f15099070168ba6cea49f9
Erymer <erymer@protonmail.com>
2026-08-04 18:58:20 -0600

    New neovim plugin checkmate

---
+++ .config/nvim/lua/magg/plugins/checkmate-nvim.lua
@@ -0,0 +1,12 @@
+return {
+  {
+  "bngarren/checkmate.nvim",
+  ft = "markdown", -- Lazy loads for Markdown files matching patterns in 'files'
+  opts = {
+    files = { 
+      "*.md",
+      "magg_checkpoint.md"
+    },
+  },
+}
+}
